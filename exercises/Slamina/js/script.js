a5d6610fa341b5310ca3152810cdfe8ef208843b
uozmann <1974155655@qq.com>
2022-01-29 13:43:22 -0500

    Fixed bug
    Ex: fixed the bug by modifying mousePressed()

--- exercises/Slamina/js/script.js
+++ exercises/Slamina/js/script.js
@@ -8,6 +8,7 @@
 //begin with the title page
 let state = `title`;
 
+//A circle to choose the animal
 let trap = {
   x: 800,
   y: 75,
@@ -15,6 +16,7 @@ let trap = {
   easing: 0.05,
 }
 
+//Arrays of animals moving horizontally
 let animals = {
   images: [],
   objects: [],
@@ -24,9 +26,9 @@ let animals = {
   vy: 150,
   vx: 0,
 };
-const ANIMAL_NUM = 10;
-const ANIMAL_PREFIX = `assets/images/animal`;
-const ANIMAL_NAMES = [
+const ANIMAL_NUM = 10; //Total amount of animal
+const ANIMAL_PREFIX = `assets/images/animal`; //Path to load the animal images
+const ANIMAL_NAMES = [ //Strings associated to each animal image in order
   "wolf",
   "cat",
   "lion",
@@ -37,17 +39,14 @@ const ANIMAL_NAMES = [
   "bear",
   "polar bear",
   "panda",
-  
-  
 ];
 
-//Text appearance
-const QUESTION_DELAY = 2000;
-// The current answer to display (we use it initially to display the click instruction)
-let currentAnswer = `Click to begin.`;
+// The current answer 
+let currentAnswer = ``;
 // The current animal name the user is trying to guess
 let currentAnimal = ``;
 
+// For background
 let colour = 200;
 
 function preload() {
@@ -62,15 +61,13 @@ function preload() {
 function setup() {
   createCanvas(1600, 600);
   
-  // Associate the animal name with its object
-  
   //Create random animals moving left and right
   for (let i=0; i< ANIMAL_NUM; i++) {
       animals.y = random(100, 600);
       animals.x = random(50, 1600);
       animals.vx = random(2,5);
-      animals.name = ANIMAL_NAMES[i];
-      let animalImg = animals.images[i];
+      animals.name = ANIMAL_NAMES[i]; // Associate the animal name with its object
+      let animalImg = animals.images[i]; // Associate the animal image with its object
       let animalObject = new Animal(animals.x, animals.y, animals.vx, animalImg, animals.name);
       animals.objects.push(animalObject);
   } 
@@ -79,10 +76,12 @@ function setup() {
   if (annyang) {
     // Create the guessing command
     let commands = {
-      'I think it is *animal': guess,
+      'Yes': function() {
+        state = 'game';
+      },
       'hello': function() {
         alert(`Hi there!`);
-      },
+      }, //To test if annyang works
     };
     // Setup annyang and start
     annyang.addCommands(commands);
@@ -111,19 +110,20 @@ function draw() {
   } 
 }
 
+//Welcome page
 function title() {
   push();
   fill(0);
   textSize(32);
   text(`Press any key to start!`, 150, 300);
   pop();
-
 }
 
+//Game page
 function game() {
-  //Setting up an easing for the ellipse
   stroke(0);
   noFill();
+  //Setting up an easing for the ellipse
   let dx = mouseX - trap.x;
   trap.x += dx * trap.easing;
   let dy = mouseY - trap.y;
@@ -138,17 +138,19 @@ function game() {
     animals.objects[i].checkOverlap();
     animals.objects[i].mousePressed()
   }
-
-  //
 }
 
 // guessing state
 function guess() {
-  // currentAnswer = animal.toLowerCase();
-  text(currentAnswer, width / 2, height / 3);
+  push();
+  fill(0);
+  textSize(32);
+  text(`Would you like to have another chance? (Respond yes if you do)`, 150, 300);
+  pop();
   displaySpeech();
 }
 
+// triggered when the guess is successful
 function ending() {
   push();
   fill(0);
@@ -158,19 +160,15 @@ function ending() {
 }
 
 //Display the text version of the speech input
-function displaySpeech() {
-  if (currentAnswer === currentAnimal) {
-    fill(0, 255, 0);
-    text(`Correct Guess!`, width / 2, height*2 / 3);
-  }
-  else {
+function displaySpeech(animal) {
+  currentAnswer = animal;
+  push();
     fill(255, 0, 0);
-    text(`Wrong Guess!`, width / 2, height*2 / 3);
-  }
-  text(currentAnswer, width / 2, height / 2);
+    text(`Wrong Guess!`, 150, 200);
+  pop();
 }
 
-//Clear out current answer
+//Clear out current answer and say another animal's name
 function nextQuestion() {
   currentAnswer = ``;
   currentAnimal = random(ANIMAL_NAMES);
@@ -180,15 +178,56 @@ function nextQuestion() {
 //Mouse Pressed Commands
 function mousePressed() {
   // check if correct animal is pressed
+  console.log(currentAnimal);
   for (let i=0; i< animals.objects.length; i++) {
     animals.objects[i].mousePressed();
-    if (animals.objects[i].found && animals.objects[i].name === currentAnimal) {
-      state = `ending`;
+    // >>>>>>>>This is my original script, where things get buggy
+    // >>>>>>>>What happen on the console.log(animals.objects[i].name) is that there is always 3-7 names named one afteranother, which renders the currentAnimal's value faulty
+    // if (animals.objects[i].found && animals.objects[i].name === currentAnimal) {
+    //   state = `ending`;
       
+    // }
+    // else if (animals.objects[i].found && animals.objects[i].name !== currentAnimal){
+    //   state = `guess`;
+    //   console.log(animals.objects[i].name);
+    // }
+
+    // So I have to do it one by one to check if the right animal is selected
+    if (animals.objects[0].found && animals.objects[0].name === currentAnimal) {
+      state = `ending`;
+    }
+    else if (animals.objects[1].found && animals.objects[1].name === currentAnimal) {
+      state = `ending`;
+    }
+    else if (animals.objects[2].found && animals.objects[2].name === currentAnimal) {
+      state = `ending`;
+    }
+    else if (animals.objects[3].found && animals.objects[3].name === currentAnimal) {
+      state = `ending`;
+    }
+    else if (animals.objects[4].found && animals.objects[4].name === currentAnimal) {
+      state = `ending`;
+    }
+    else if (animals.objects[5].found && animals.objects[5].name === currentAnimal) {
+      state = `ending`;
+    }
+    else if (animals.objects[6].found && animals.objects[6].name === currentAnimal) {
+      state = `ending`;
+    }
+    else if (animals.objects[7].found && animals.objects[7].name === currentAnimal) {
+      state = `ending`;
+    }
+    else if (animals.objects[8].found && animals.objects[8].name === currentAnimal) {
+      state = `ending`;
+    }
+    else if (animals.objects[9].found && animals.objects[9].name === currentAnimal) {
+      state = `ending`;
     }
     else if (animals.objects[i].found && animals.objects[i].name !== currentAnimal){
-      state = `guess`;
-    }
+      console.log(animals.objects[i].name);
+        state = `guess`;
+        console.log(animals.objects[i].name);
+      }
   }
   
 }

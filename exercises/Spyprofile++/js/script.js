35a6d049fd654575439211ee7ae6cfdd8df8489d
uozmann <1974155655@qq.com>
2022-02-02 01:03:26 -0500

    cleaning the codes

--- exercises/Spyprofile++/js/script.js
+++ exercises/Spyprofile++/js/script.js
@@ -19,6 +19,7 @@ let playerEZodiac = ``;
 
 
 function preload() {
+  //load the zodiac data
   zodiac = loadJSON("assets/data/zodiac.json"); 
 }
 function setup() {
@@ -40,26 +41,28 @@ function setup() {
 
 function draw() {
   background(200);
-  robot();
-  conversationText();
+  robot(); //draw the player's face
+  conversationText(); //add the texts
   console.log(qNum);
 }
 
+//create the face of the player
 function robot() {
   push();
-  fill(255-colour,230-colour,190-colour, transparency);
-  ellipse(width/4, height/2, 400);
+  fill(255-colour,230-colour,190-colour, transparency); //varying depending on the player's answer
+  ellipse(width/4, height/2, 400); //head
   fill(0);
-  ellipse(width/4 - 100, height/2, 50);
+  ellipse(width/4 - 100, height/2, 50); //eye
   ellipse(width/4 + 100, height/2, 50);
   textAlign(CENTER, CENTER);
   textSize(100);
-  text(mouthShape, width/4, height/2 + 100);
-  text(playerName, width/4, height/2 + 300);
+  text(mouthShape, width/4, height/2 + 100); //mouth
+  text(playerName, width/4, height/2 + 300); //text displaying the player's name
   pop();
 }
 
 function conversationText() {
+  //instruction texts
   push();
   fill(0);
   textSize(32);
@@ -68,22 +71,20 @@ function conversationText() {
   text(`Answer the "what is your" with complete sentence. `, width/2, height/3);
   text(`Ex. My name is XXX.`, width/2, height/3 + 25);
   text(`You may proceed to the next question only if you answered the previous one.`, width/2, height/3 + 50);
-  text(`Note that the machine is dumb. You may need to repeat yourself.`, width/2, height/3 + 75);
+  text(`Note that the machine is very very dumb. You may need to repeat yourself.`, width/2, height/3 + 75);
   pop();
 
-  // console.log(playerWZodiac.keywords);
+  //display the player's secret profile based on the given answers
   if (qNum === 5) {
   text(`You are a very ` + playerWZodiac.keywords + ` ` + adj + ` person.`, width/2, height/3 + 175);
   text(`You now live in ` + playerEZodiac.element + ` because you are born in ` + playerWZodiac.ruling_body_modern, width/2, height/3 + 200);
   text(`You are the ` + playerWZodiac.gloss + ` of the ` + playerEZodiac.direction, width/2, height/3 + 225);
   text(`Your mysterious surname is ` + playerEZodiac.unicode_symbol + `儿`, width/2, height/3 + 250);
-  }
-  
+  } 
 }
 
 function askQuestion() {
-  sentence = `what is your ${content}?`;
-
+  // Ask different questions depending on the question number
   if (qNum === 0) {
     content = "name";
     // qNum = 1;
@@ -105,12 +106,15 @@ function askQuestion() {
     qNum =5;
   }
 
+  //Sentence the computer will say
+  sentence = `what is your ${content}?`;
   responsiveVoice.speak(sentence, "UK English Female", {
-    onstart: speaking,
-    onend: notSpeaking
+    onstart: speaking, //add mouth animation
+    onend: notSpeaking //stop mouth animation
   });
 }
 
+//mouth animation
 function speaking() {
   if (mouthShape === `o`) { 
     setTimeout(function (){mouthShape = `O`;}, 1000);
@@ -120,19 +124,25 @@ function speaking() {
   }
 }
 
+//freeze mouth animation
 function notSpeaking() {
   mouthShape = `O`;
 }
 
+//annyang detecting answer to question
 function setName(name) {
   playerName = name;
   qNum =1;
   console.log(playerName);
 }
 
+//annyang detecting answer to question
 function setColour(skinColour) {
   qNum =2;
+  //set skin colour as an adjective for a sentence
   adj = skinColour;
+
+  //colour the player's face
   transparency = 255;
   if (skinColour === 'white') {
     colour = 0;
@@ -146,14 +156,15 @@ function setColour(skinColour) {
   else if (skinColour === 'black') {
     colour = 100;
   }
-  
 }
 
+//annyang detecting answer to question
 function setWZodiac(wZodiac) {
   qNum =3;
   // playerWZodiac = zodiac.western_zodiac.${wZodiac};
   console.log(wZodiac);
 
+  //detecting player's western zodiac sign
   if (wZodiac === 'Aries') {
     playerWZodiac = zodiac.western_zodiac.Aries;
   }
@@ -189,13 +200,15 @@ function setWZodiac(wZodiac) {
   }
   else if (wZodiac === 'Pisces') {
     playerWZodiac = zodiac.western_zodiac.Gemini;
-  }
-  
+  } 
 }
 
+//annyang detecting answer to question
 function setEZodiac(eZodiac) {
   qNum =4;
   console.log(eZodiac);
+
+  //determining the player's eastern zodiac sign
   if (eZodiac === 'rat' || eZodiac === 'mouse') {
     playerEZodiac = zodiac.eastern_zodiac.Rat;
   }
@@ -234,11 +247,7 @@ function setEZodiac(eZodiac) {
   }
 }
 
+//Prompt question when the user mouse press
 function mousePressed() {
   askQuestion();
-  // loadJSON("assets/data/zodiac.json", setJson);
-}
-
-function setJson(json) {
-  // zodiac = json;
 }

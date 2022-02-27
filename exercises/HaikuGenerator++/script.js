28f7ff5ec63011639b6aec04ed3e409e5a711f9d
uozmann <1974155655@qq.com>
2022-02-27 17:11:15 -0500

    Ex6: Added tree image
    Added functionalities for the tree image

---
+++ exercises/HaikuGenerator++/script.js
@@ -0,0 +1,154 @@
+/**
+Haiku Generator
+CART 263 Exercise
+A program that generates a random haiku based on pre-existing arrays
+of lines of the correct syllable length. Also swaps out lines if the user
+clicks on them with a fade in and out effect.
+*/
+
+"use strict";
+
+// Our pre-made haiku lines
+let haikuLines = {
+  fiveSyllables: [
+    `O, to be a tree`,
+    `The cat does not know`,
+    `We are all forests`,
+    `You have done your best`,
+    `They are all gone now`
+  ],
+  sevenSyllables: [
+    `Say the things left unsaid`,
+    `Never believe the wind's lies`,
+    `The autumn stretches its legs`,
+    `Nothing can satisfy you`,
+    `They will not come back again`
+  ]
+};
+
+// Our three elements on the page that contain each line of the poem
+let line1 = document.getElementById(`line-1`);
+let line2 = document.getElementById(`line-2`);
+let line3 = document.getElementById(`line-3`);
+let customCursor = document.getElementById(`cursor`);
+let tree = document.getElementById(`tree`);
+
+
+// Set up the starting lines
+setupLines();
+// Listen for clicks on each element and respond by changing them
+addListeners();
+/**
+Puts a randomly chosen haiku line in each line of the poem in HTML
+*/
+function setupLines() {
+  line1.innerText = random(haikuLines.fiveSyllables);
+  line2.innerText = random(haikuLines.sevenSyllables);
+  line3.innerText = random(haikuLines.fiveSyllables);
+}
+
+/**
+Adds event listeners for changing each line of the poem
+*/
+function addListeners() {
+  line1.addEventListener(`click`, changeLine);
+  line2.addEventListener(`click`, changeLine);
+  line3.addEventListener(`click`, changeLine);
+  window.addEventListener(`mousemove`, myCursor);
+}
+
+function myCursor(e) {
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+    customCursor.style.top = `${mouseY + 100}px`;
+    customCursor.style.left = `${mouseX + 100}px`;
+}
+
+/**
+Triggers a fade out when a line is clicked
+*/
+function changeLine(event) {
+  fadeOut(event.target, 1, 2);
+  customCursor.innerText = line1.innerText;
+  if (customCursor.innerText === line3.innerText) {
+    tree.style.opacity = 1;
+    } else {
+    tree.style.opacity = 0;
+    }
+}
+
+/**
+Reduces the opacity of the provided element until it reaches zero
+then changes its line and triggers a fade in
+*/
+function fadeOut(element, opacity, padding) {
+  // Change the opacity of the line
+  opacity -= 0.01;
+  padding += 0.01;
+  element.style[`opacity`] = opacity;
+  element.style[`padding`] = `${padding}rem`;
+  // Check if the opacity is greater than 0...
+  if (opacity > 0 && padding < 2) {
+    // If so, keep fading on the next frame
+    // Note the use of an anonymous function here so we can pass
+    // arguments to fadeOut()
+    requestAnimationFrame(function() {
+      fadeOut(element, opacity, padding);
+    });
+  }
+  else {
+    // If not, we can switch lines and fade in...
+    // Set a new line of poem for the element
+    setNewLine(element);
+    // Trigger a fade in
+    fadeIn(element, 0, 0);
+  }
+}
+
+/**
+Increases the opacity of the provided element until it reaches
+1 and then stops.
+*/
+function fadeIn(element, opacity, padding) {
+  // Increase the opacity
+  opacity += 0.01;
+  padding -= 0.01;
+  element.style[`opacity`] = opacity;
+  element.style[`padding`] = `${padding}rem`;
+  // Check if opacity is still less than 1
+  if (opacity < 1) {
+    // Keep fading. Note the use of an anonymous function here so we
+    // can pass arguments to fadeIn()
+    requestAnimationFrame(function() {
+      fadeIn(element, opacity, padding);
+    });
+  }
+  else {
+    // Do nothing - we're done!
+  }
+}
+
+/**
+Sets the text of the element to a randomly chosen haiku line, accounting for
+syllables
+*/
+function setNewLine(element) {
+  if (element === line1 || element === line3) {
+    // If the element is line1 or line3, use five syllables
+    element.innerText = random(haikuLines.fiveSyllables);
+  }
+  else {
+    // If the element is line2 use seven
+    element.innerText = random(haikuLines.sevenSyllables);
+  }
+}
+
+/**
+A helper function that returns a random element from the provided array
+*/
+function random(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+
+

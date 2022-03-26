1ef96002395898cdcca090f7368d5facd841270d
uozmann <1974155655@qq.com>
2022-03-25 23:28:21 -0400

    E9: Added Score count
    Added a variable (points) to count the points

--- exercises/DesperatelySeekingSadness++/js/script.js
+++ exercises/DesperatelySeekingSadness++/js/script.js
@@ -3,7 +3,7 @@ Desperately Seeking Sadness
 CART 263
 
 New elements:
-1. Two player game: one player controles the avatar, the other one controles the thumb down in handleInput()
+1. Try to touch the thumb down one player controles the avatar, the other one controles the thumb down in handleInput() through the same keys.
 2.  Score count: add a point at getSad() in the Play class
 3. Settled the sad emoji to collide with the happiness and game frame.
 4. Added a sound effect to the collision with the sad emoji
@@ -27,4 +27,14 @@ let game = new Phaser.Game(config);
 
 
 // Dom manipulation
+let points = 0;
+const score = document.getElementById(`score`);
+let audioGetSadness =  new Audio(`assets/sounds/bark.wav`); 
 
+//Called by the getSadness() at Play.js
+function updatePoints() {
+   /* the audio is now playable; play it if permissions allow */
+  audioGetSadness.play();
+  score.textContent = `${points}`;
+    
+}
